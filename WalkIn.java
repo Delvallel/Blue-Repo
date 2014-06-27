f8ee417fbdc6a9ee0e19d9dc1eda3f1d79018abf
Delvallel <delvallelance@yahoo.com>
2014-06-27 22:19:37 +0800

    Create WalkIn.java

---
+++ WalkIn.java
@@ -0,0 +1,37 @@
+package Logic;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Date;
+import java.util.List;
+/**
+ *
+ * @author Win 7
+ */
+public class WalkIn {
+    private Date dateOfPurchase;
+    private String inVoice;
+
+    public WalkIn(Date dateOfPurchase) {
+       this.dateOfPurchase = dateOfPurchase; 
+    }
+    
+    public List<Transaction> getTransaction(){
+    return null;
+    }
+    
+    public void addTransaction(Transaction t){
+    
+    }
+
+
+    
+    
+
+    
+    
+    
+}
