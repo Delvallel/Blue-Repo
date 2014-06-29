acea5265a72037ca533599cfee48f03426ebf331
Delvallel <delvallelance@yahoo.com>
2014-06-29 11:04:53 +0800

    Create DaoInterface.java

---
+++ DaoInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Win 7
+ */
+
+import java.util.ArrayList;
+
+
+public interface DaoInterface {
+
+   
+            
+    
+      
+}
