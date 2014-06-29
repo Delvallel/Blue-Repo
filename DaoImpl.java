b22cd401fa4e8f0327a3d4843c4a6f225fd3eba1
Delvallel <delvallelance@yahoo.com>
2014-06-29 11:03:56 +0800

    Create DaoImpl.java

---
+++ DaoImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Win 7
+ */
+
+import Interfaces.*;
+import Connection.DBconnection;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DaoImpl implements DaoInterface {
+
+    private Connection connection;
+    private DBconnection dBConnection;
+
+   
+
+   
+
+}
